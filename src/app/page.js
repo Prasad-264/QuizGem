b1737c80e6a3d72afee4fdaf6f428150a261cd98
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-09-10 17:35:38 +0530

    removed unused code

--- src/app/page.js
+++ src/app/page.js
@@ -8,111 +8,6 @@ import Loading from "@/components/Loading";
 import Footer from "@/components/Footer";
 
 export default function Home() {
-  // const [quiz, setQuiz] = useState({
-  //   "title": "SOLID Principles Quiz",
-  //   "quizzes": [
-  //       {
-  //           "question": "What does the 'S' in SOLID stand for?",
-  //           "options": [
-  //               "Single Responsibility",
-  //               "Simple",
-  //               "Structured",
-  //               "Scalable"
-  //           ],
-  //           "correct_option": "Single Responsibility"
-  //       },
-  //       {
-  //           "question": "Which principle promotes creating classes with a single, well-defined purpose?",
-  //           "options": [
-  //               "Open/Closed Principle",
-  //               "Liskov Substitution Principle",
-  //               "Interface Segregation Principle",
-  //               "Single Responsibility Principle"
-  //           ],
-  //           "correct_option": "Single Responsibility Principle"
-  //       },
-  //       {
-  //           "question": "The Open/Closed Principle suggests that software entities should be:",
-  //           "options": [
-  //               "Open for extension, closed for modification",
-  //               "Open for modification, closed for extension",
-  //               "Always open for both extension and modification",
-  //               "None of the above"
-  //           ],
-  //           "correct_option": "Open for extension, closed for modification"
-  //       },
-  //       {
-  //           "question": "Which principle states that subtypes should be substitutable for their base types without altering the correctness of the program?",
-  //           "options": [
-  //               "Interface Segregation Principle",
-  //               "Liskov Substitution Principle",
-  //               "Dependency Inversion Principle",
-  //               "Open/Closed Principle"
-  //           ],
-  //           "correct_option": "Liskov Substitution Principle"
-  //       },
-  //       {
-  //           "question": "The Interface Segregation Principle advocates for:",
-  //           "options": [
-  //               "Creating large, comprehensive interfaces",
-  //               "Designing small, specific interfaces",
-  //               "Using only abstract classes",
-  //               "Avoiding interfaces altogether"
-  //           ],
-  //           "correct_option": "Designing small, specific interfaces"
-  //       },
-  //       {
-  //           "question": "What does the 'D' in SOLID stand for?",
-  //           "options": [
-  //               "Design",
-  //               "Dependency",
-  //               "Data",
-  //               "Development"
-  //           ],
-  //           "correct_option": "Dependency"
-  //       },
-  //       {
-  //           "question": "Which principle suggests depending on abstractions, not concretions?",
-  //           "options": [
-  //               "Liskov Substitution Principle",
-  //               "Dependency Inversion Principle",
-  //               "Interface Segregation Principle",
-  //               "Open/Closed Principle"
-  //           ],
-  //           "correct_option": "Dependency Inversion Principle"
-  //       },
-  //       {
-  //           "question": "What is the primary benefit of adhering to SOLID principles?",
-  //           "options": [
-  //               "Increased code complexity",
-  //               "Reduced maintainability",
-  //               "Improved code readability and maintainability",
-  //               "None of the above"
-  //           ],
-  //           "correct_option": "Improved code readability and maintainability"
-  //       },
-  //       {
-  //           "question": "How can SOLID principles help with software evolution?",
-  //           "options": [
-  //               "Making it harder to adapt to changing requirements",
-  //               "Facilitating easier modifications and extensions",
-  //               "Having no impact on software evolution",
-  //               "None of the above"
-  //           ],
-  //           "correct_option": "Facilitating easier modifications and extensions"
-  //       },
-  //       {
-  //           "question": "Which of the following is NOT a SOLID principle?",
-  //           "options": [
-  //               "Single Responsibility Principle",
-  //               "Liskov Substitution Principle",
-  //               "Dependency Injection Principle",
-  //               "Interface Segregation Principle"
-  //           ],
-  //           "correct_option": "Dependency Injection Principle"
-  //       }
-  //   ]
-  // });
   const [quiz, setQuiz] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -120,8 +15,7 @@ export default function Home() {
     setLoading(true);
     try {
       const quizData = await generateQuiz(url);
-      setQuiz(quizData);     
-      console.log(quizData);
+      setQuiz(quizData);
     } catch (e) {
       setError("Failed to generate quiz. Please try again.");
     }
